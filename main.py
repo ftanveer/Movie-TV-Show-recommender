c9c39ac0797b852cec49f41fb464ac36222db281
ftanveer <farhanxperia@gmail.com>
2023-02-13 16:15:58 -0500

    Initialization

---
+++ main.py
@@ -0,0 +1,199 @@
+import streamlit as st
+import pickle
+import requests
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+######## File Reading #########
+
+movie_names_desc = pickle.load(open('movie_names_desc.pkl','rb'))
+
+df_1 = pickle.load(open('df_1.pkl','rb'))
+df_2_nlp = pickle.load(open('df_2_nlp.pkl','rb'))
+df_3_nlp = pickle.load(open('df_3_nlp.pkl','rb'))
+
+### Generating cosine similarities  #####
+
+
+tfidf = TfidfVectorizer()
+
+#vectorize the processed text
+
+tfidf_matrix = tfidf.fit_transform(df_2_nlp["description"])
+
+
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+#
+count = CountVectorizer()
+count_matrix = count.fit_transform(df_3_nlp['genres_soup'])
+
+
+cosine_sim_metadata = cosine_similarity(count_matrix, count_matrix)
+#
+hybrid_cosine = cosine_sim + cosine_sim_metadata
+
+##### Get Posters #########
+
+def get_poster(imdb_id):
+    try:
+        url = f'https://api.themoviedb.org/3/find/{imdb_id}?api_key={api_key}&language=en-US&external_source=imdb_id'
+        response = requests.get(url)
+        data = response.json()
+        movie = data["movie_results"][0]
+        poster = movie["poster_path"]
+
+        poster_url = f'https://image.tmdb.org/t/p/original/{poster}'
+    except:
+        poster_url = "No Image Available"
+
+    #actual_poster = st.image(poster_url, width=200)
+    return poster_url
+
+
+######## Recommendation functions #########
+
+def recommendation_engine(movie_title, cosine_similar):
+    recommended_movies = []
+    movie_desc_list = []
+    imdb_ids = []
+    streaming_platform_list = []
+    cost_list = []
+
+    index = movie_names_desc[movie_names_desc['title'] == movie_title].index[0]
+
+    similarity_score = pd.Series(cosine_similar[index]).sort_values(ascending=False)
+
+    top_10_content = (similarity_score.iloc[1:11].index)
+
+    ## if movie name is  duplicated then it exists in multiple streaming platforms, hence we need
+    ## to drop a duplcate name and mention both streaming platforms. We also need to add another
+    ##recommendation to make up for the duplicate
+
+    for i in top_10_content:
+        recommended_movies.append(list(df_1['title'])[i])
+        movie_desc_list.append(list(df_1['description'])[i])
+        imdb_ids.append(list(df_1['imdb_id'])[i])
+        streaming_platform_list.append(list(df_1['streaming_service'])[i])
+        cost_list.append(list(df_1['subscription_cost'])[i])
+
+
+    recommended_dict = {'Movie Name': recommended_movies, "Plot": movie_desc_list,"imdb_id":  imdb_ids, "streaming_service":streaming_platform_list,
+                        "subscription_cost": cost_list}
+    recommended_df = pd.DataFrame.from_dict(recommended_dict)
+    recommended_df["posters"] = recommended_df["imdb_id"].apply(get_poster)
+
+    # for recom in recommendations["imdb_id"].values:
+    #     movie_poster = get_poster(recom)
+    #     st.image(movie_poster, width=200)
+    return recommended_df
+
+
+
+
+api_key = "25b4bc2ccb155d6b14e2276114009e03"
+
+
+
+######## UX Design #########
+
+background =  'background.jpeg'
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1585314062340-f1a5a7c9328d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
+
+st.markdown("<h1 style='text-align: center; color: orange;'>Movie Recommender System</h1>", unsafe_allow_html=True)
+
+all_content_names =  movie_names_desc['title'].values
+
+content = st.selectbox("Search for a Movie / TV Show", all_content_names)
+
+
+
+############################## MAIN FUNCTION ##############
+
+
+if st.button('Recommend'):
+  recommendations = recommendation_engine(content,hybrid_cosine)
+  st.write("You might also like:")
+
+  for index, row in recommendations.iterrows():
+
+      ###### Movie Name
+
+      movie_name = row["Movie Name"]
+
+      st.markdown(
+          f"<h2 style='color: orange;'>  {movie_name} </h2>",
+          unsafe_allow_html=True,
+      )
+
+      ###### Poster Images
+
+      try:
+          st.image(row["posters"], width = 200)
+      except:
+          st.write("No preview available")
+
+      ###### Plot
+
+      plot = row["Plot"]
+      st.markdown(
+          f"<p style='color: orange;'> {plot} </p>",
+          unsafe_allow_html=True,
+      )
+      ###### Streaming Service
+      streaming_platform = row["streaming_service"]
+      st.markdown(
+          f"<p style='color: orange;'> {streaming_platform} </p>",
+          unsafe_allow_html=True,
+      )
+
+      cost = row["subscription_cost"]
+      st.markdown(
+          f"<p style='color: orange;'> {cost} </p>",
+          unsafe_allow_html=True,
+      )
+
+
+
+
+
+
+
+
+######## Side bar widget #########
+
+# page = 0
+
+# def main():
+#     st.sidebar.title("Navigation")
+#     global page
+#     page = st.sidebar.radio("Choose a page", [0,1,2])
+#
+#     if page == 0:
+#         st.write("You are on page 0")
+#     elif page == 1:
+#         st.write("You are on page 1")
+#     elif page == 2:
+#         st.write("You are on page 2")
+# if __name__  == '__main__':
+#     main()
+

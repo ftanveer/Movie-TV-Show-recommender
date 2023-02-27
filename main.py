e8d8dc0d65427d19d013de54d4470a0fe45e037c
Farhan Hasan <107018730+ftanveer@users.noreply.github.com>
2023-02-27 17:10:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import os
 
 
 try:
-    #API_KEY = str(os.environ.get("MY_API_KEY")) 
-    API_KEY : '${{ secrets.MY_API_KEY }}'
+    API_KEY = os.environ.get("MY_API_KEY")
+    #API_KEY : '${{ secrets.MY_API_KEY }}'
     st.write("found!")
 except:
     st.write("not obtained")
